d17b5d6d41f10100bbc2dc3c4df80fde3ef148e6
Novita8407 <novitaramadhaniramadhani9@gmail.com>
2023-10-04 16:31:14 +0700

    add tugas_1

---
+++ Tugas_1/Soal.js
@@ -0,0 +1,16 @@
+let Nama = "Novita";
+let Usia = 19;
+let Tinggi = 155;
+
+let bilangan1 = 6;
+let bilangan2 = 9;
+let bilangan3 = 7;
+
+let isStudent = true;
+let isStudent1 = false;
+
+console.log("hasil penjumlahan=", bilangan1 + bilangan2);
+console.log("hasil pengurangan=", bilangan2 - bilangan3);
+console.log("usia dan tinggi=", Usia * Tinggi);
+console.log(isStudent);
+console.log(isStudent1);
